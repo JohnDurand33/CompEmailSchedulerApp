c6d946ab2a42e55072efed04063c56e46e6b1f31
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-08-01 18:11:15 -0600

    updates to test app functionality in place

--- app.py
+++ app.py
@@ -26,7 +26,9 @@ def create_app():
     @app.route('/send-test-email')
     def send_test_email():
         try:
-            send_email('You Are THE BEST!!!!!', 'john.durand1@gmail.com','You are amazing, and I love you for everything you do every day.  Thanks for being my Best Friend-    John')
+            attachment_path = os.path.join(app.root_path, 'path_to_your_miro_workflow_file.pdf')
+            send_email('OMG I LOVE YOUUUUUUUUUUUUUUUU', 'john.durand1@gmail.com',
+                       "Today I built about half of the application logic.  I'm mapping it al out so doing the front and back end at the same time.  Learning sooooo much.  If I could attach it here it would be awesome-   John", attachment_path=attachment_path)
             return jsonify({"msg": "Email sent successfully"}), 200
         except Exception as e:
             return jsonify({"msg": str(e)}), 500
--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
 from flask_mail import Message
 from flask import current_app
+import os
 
 
-def send_email(subject, recipient, body):
+def send_email(subject, recipient, body, attachment_path=None):
     mail = current_app.extensions.get('mail')
     if not mail:
         raise RuntimeError('Mail extension not initialized')
     msg = Message(subject, recipients=[recipient], body=body, sender="john.durand1@gmail.com")
+
+    if attachment_path and os.path.exists(attachment_path):
+        with current_app.open_resource(attachment_path) as fp:
+            msg.attach(os.path.basename(attachment_path),
+                       "application/pdf", fp.read())
+
     mail.send(msg)
